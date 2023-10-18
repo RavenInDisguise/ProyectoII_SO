51699ea71a892871998061fa2e9cabccdc73994b
Mónica Alfaro Parrales <monicaalfarop@estudiantec.cr>
2023-10-17 19:16:46 -0600

    Added delete and logic related to extracting all files

--- star.c
+++ star.c
@@ -4,31 +4,37 @@
 #include <stdbool.h>
 #include "verbose.h"
 
-#define MAX_FILE_NAME 200 //TUVE QUE SUBIRLE AL LIMITE PARA HACER PRUEBAS CON NUESTROS FILES
+#define MAX_FILE_NAME 200 // TUVE QUE SUBIRLE AL LIMITE PARA HACER PRUEBAS CON NUESTROS FILES
 #define MAX_ARCHIVE_SIZE 100
 
 // Estructura para el encabezado de archivo en el archivo de salida
-struct FileHeader {
+struct FileHeader
+{
     char name[MAX_FILE_NAME];
     unsigned int size;
+    bool deleted; // Me permite conocer si este file fue eliminado; ELIMINADO = T
 };
 
-void create_tar(const char *outputFile, int numFiles, char *inputFiles[]) {
+void create_tar(const char *outputFile, int numFiles, char *inputFiles[])
+{
     verbose("Nombre del archivo de salida: %s\n", outputFile);
 
     FILE *outFile = fopen(outputFile, "wb");
-    if (!outFile) {
+    if (!outFile)
+    {
         perror("Error al abrir el archivo de salida\n");
         exit(1);
     }
 
     vverbose("Abriendo los archivos de entrada...\n");
 
-    for (int i = 0; i < numFiles; i++) {
+    for (int i = 0; i < numFiles; i++)
+    {
 
         vverbose("Leyendo %s en memoria...\n", inputFiles[i]);
         FILE *inputFile = fopen(inputFiles[i], "rb");
-        if (!inputFile) {
+        if (!inputFile)
+        {
             perror("Error al abrir el archivo de entrada\n");
             fclose(outFile);
             exit(1);
@@ -39,19 +45,18 @@ void create_tar(const char *outputFile, int numFiles, char *inputFiles[]) {
         long fileSize = ftell(inputFile);
         rewind(inputFile);
 
-
         vverbose("Creando cabecera de metadatos...\n");
         struct FileHeader header;
         strncpy(header.name, inputFiles[i], sizeof(header.name));
         header.size = (unsigned int)fileSize;
         fwrite(&header, sizeof(struct FileHeader), 1, outFile);
 
-
         vverbose("Copiando datos al paquete %s...\n", outputFile);
         char *buffer;
         buffer = (char *)calloc(fileSize, sizeof(char));
         size_t bytesRead;
-        while ((bytesRead = fread(buffer, 1, fileSize, inputFile)) > 0) {
+        while ((bytesRead = fread(buffer, 1, fileSize, inputFile)) > 0)
+        {
             fwrite(buffer, 1, bytesRead, outFile);
         }
 
@@ -65,24 +70,29 @@ void create_tar(const char *outputFile, int numFiles, char *inputFiles[]) {
     verbose("Se creó el archivo star: %s\n", outputFile);
 }
 
-void extract_tar(const char *archiveFile, int numFiles, char *filesToExtract[]) {
-    //Implementa logica de extraer
-
+void extract_tar(const char *archiveFile, int numFiles, char *filesToExtract[])
+{
     FILE *tarFile = fopen(archiveFile, "rb");
-    if (!tarFile) {
+    if (!tarFile)
+    {
         perror("Error al abrir el archivo tar");
         exit(1);
     }
 
-    for (int i = 0; i < numFiles; i++) {
-        bool found = false;
+    if (numFiles == 0)
+    {
+        // No se especificaron archivos específicos para extraer,
+        // por lo que extraeremos todos los archivos del .tar.
+        verbose("Extrayendo todos los archivos del archivo tar: %s\n", archiveFile);
         struct FileHeader header;
-
-        while (fread(&header, sizeof(struct FileHeader), 1, tarFile) == 1) {
-            if (strcmp(header.name, filesToExtract[i]) == 0) {
-                printf("Extrayendo: %s\n", header.name);
+        while (fread(&header, sizeof(struct FileHeader), 1, tarFile) == 1)
+        {
+            if (!header.deleted)
+            {
+                vverbose("Extrayendo: %s\n", header.name);
                 FILE *outputFile = fopen(header.name, "wb");
-                if (!outputFile) {
+                if (!outputFile)
+                {
                     perror("Error al crear el archivo de salida");
                     fclose(tarFile);
                     exit(1);
@@ -92,7 +102,8 @@ void extract_tar(const char *archiveFile, int numFiles, char *filesToExtract[]) 
                 buffer = (char *)calloc(header.size, sizeof(char));
                 size_t bytesRead;
 
-                while (header.size > 0) {
+                while (header.size > 0)
+                {
                     size_t readSize = (header.size > sizeof(buffer)) ? sizeof(buffer) : header.size;
                     bytesRead = fread(buffer, 1, readSize, tarFile);
                     fwrite(buffer, 1, bytesRead, outputFile);
@@ -101,67 +112,170 @@ void extract_tar(const char *archiveFile, int numFiles, char *filesToExtract[]) 
 
                 free(buffer);
                 fclose(outputFile);
-                found = true;
-                break;
-            } else {
-                // Si no es el archivo a extraer, avanzamos el puntero
-                fseek(tarFile, header.size, SEEK_CUR);
             }
         }
+    }
+    else
+    {
+        // Se especificaron archivos específicos para extraer.
+        verbose("Extrayendo archivos especificados del archivo tar: %s\n", archiveFile);
+        for (int i = 0; i < numFiles; i++)
+        {
+            bool found = false;
+            struct FileHeader header;
 
-        if (!found) {
-            printf("Archivo no encontrado: %s\n", filesToExtract[i]);
+            while (fread(&header, sizeof(struct FileHeader), 1, tarFile) == 1)
+            {
+                if (!header.deleted)
+                {
+                    if (strcmp(header.name, filesToExtract[i]) == 0)
+                    {
+                        vverbose("Extrayendo: %s\n", header.name);
+                        FILE *outputFile = fopen(header.name, "wb");
+                        if (!outputFile)
+                        {
+                            perror("Error al crear el archivo de salida");
+                            fclose(tarFile);
+                            exit(1);
+                        }
+
+                        char *buffer;
+                        buffer = (char *)calloc(header.size, sizeof(char));
+                        size_t bytesRead;
+
+                        while (header.size > 0)
+                        {
+                            size_t readSize = (header.size > sizeof(buffer)) ? sizeof(buffer) : header.size;
+                            bytesRead = fread(buffer, 1, readSize, tarFile);
+                            fwrite(buffer, 1, bytesRead, outputFile);
+                            header.size -= bytesRead;
+                        }
+
+                        free(buffer);
+                        fclose(outputFile);
+                        found = true;
+                        break;
+                    }
+                    else
+                    {
+                        fseek(tarFile, header.size, SEEK_CUR);
+                    }
+                }
+            }
+
+            if (!found)
+            {
+                printf("Archivo no encontrado: %s\n", filesToExtract[i]);
+            }
         }
     }
 
     fclose(tarFile);
-    printf("Extraer archivos de %s\n", archiveFile);
+    verbose("Extracción completada del archivo tar: %s\n", archiveFile);
 }
 
-void list_tar(const char *archiveFile) {
+void list_tar(const char *archiveFile)
+{
     FILE *tarFile = fopen(archiveFile, "rb");
-    if (!tarFile) {
+    if (!tarFile)
+    {
         perror("Error al abrir el archivo tar");
         exit(1);
     }
 
     struct FileHeader header;
-    while (fread(&header, sizeof(struct FileHeader), 1, tarFile) == 1) {
-        printf("Nombre de archivo: %s\n", header.name);
-        verbose("Tamaño del archivo: %u bytes\n", header.size);
-
+    while (fread(&header, sizeof(struct FileHeader), 1, tarFile) == 1)
+    {
+        if (!header.deleted)
+        {
+            printf("Nombre de archivo: %s\n", header.name);
+            verbose("Tamaño del archivo: %u bytes\n", header.size);
+        }
         fseek(tarFile, header.size, SEEK_CUR);
     }
 
     fclose(tarFile);
-    printf("Listar contenido de %s\n", archiveFile);
+    printf("Listado de contenido completado de %s\n", archiveFile);
 }
 
-void delete_tar(const char *archiveFile, int numFiles, char *filesToDelete[]) {
-    verbose("Borrar archivos de %s\n", archiveFile);
+void delete_tar(const char *archiveFile, int numFiles, char *filesToDelete[])
+{
+    FILE *tarFile = fopen(archiveFile, "rb+");
+    if (!tarFile)
+    {
+        perror("Error al abrir el archivo tar");
+        exit(1);
+    }
+
+    verbose("Borrando archivos de %s\n", archiveFile);
+
+    struct FileHeader header;
+    long fileOffset = 0;
+
+    while (fread(&header, sizeof(struct FileHeader), 1, tarFile) == 1)
+    {
+        if (!header.deleted)
+        {
+            bool found = false;
+            for (int i = 0; i < numFiles; i++)
+            {
+                if (strcmp(header.name, filesToDelete[i]) == 0)
+                {
+                    vverbose("Borrando: %s\n", header.name);
+                    header.deleted = true;
+                    fseek(tarFile, fileOffset, SEEK_SET);
+                    fwrite(&header, sizeof(struct FileHeader), 1, tarFile);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                fileOffset = ftell(tarFile);
+                fileOffset += header.size;
+                fseek(tarFile, fileOffset, SEEK_SET);
+            }
+        }
+        else
+        {
+            // Salta los archivos marcados como borrados
+            fileOffset = ftell(tarFile);
+            fileOffset += header.size;
+            fseek(tarFile, fileOffset, SEEK_SET);
+        }
+    }
+
+    fclose(tarFile);
+    verbose("Borrado completado en el archivo tar: %s\n", archiveFile);
 }
 
-void update_tar(const char *archiveFile, int numFiles, char *filesToUpdate[]) {
+void update_tar(const char *archiveFile, int numFiles, char *filesToUpdate[])
+{
     verbose("Actualizar archivo tar: %s\n", archiveFile);
 }
 
 /*
-* TODO: implementar logica de buscar hueco donde quepa el archivo nuevo
-*/
-void append_tar(const char *archiveFile, int numFiles, char *filesToAppend[]){
+ * TODO: implementar logica de buscar hueco donde quepa el archivo nuevo
+ */
+void append_tar(const char *archiveFile, int numFiles, char *filesToAppend[])
+{
     verbose("Añadiendo archivos al paquete %s\n", archiveFile);
 
     FILE *outFile = fopen(archiveFile, "ab");
-    if (!outFile) {
+    if (!outFile)
+    {
         perror("Error al abrir el archivo de salida\n");
         exit(1);
     }
 
-    for (int i = 0; i < numFiles; i++) {
+    for (int i = 0; i < numFiles; i++)
+    {
 
         verbose("Leyendo %s en memoria...\n", filesToAppend[i]);
         FILE *inputFile = fopen(filesToAppend[i], "rb");
-        if (!inputFile) {
+        if (!inputFile)
+        {
             perror("Error al abrir el archivo de entrada\n");
             fclose(outFile);
             exit(1);
@@ -172,19 +286,18 @@ void append_tar(const char *archiveFile, int numFiles, char *filesToAppend[]){
         long fileSize = ftell(inputFile);
         rewind(inputFile);
 
-
         verbose("Creando cabecera de metadatos...\n");
         struct FileHeader header;
         strncpy(header.name, filesToAppend[i], sizeof(header.name));
         header.size = (unsigned int)fileSize;
         fwrite(&header, sizeof(struct FileHeader), 1, outFile);
 
-
         verbose("Copiando datos al paquete %s...\n", archiveFile);
         char *buffer;
         buffer = (char *)calloc(fileSize, sizeof(char));
         size_t bytesRead;
-        while ((bytesRead = fread(buffer, 1, fileSize, inputFile)) > 0) {
+        while ((bytesRead = fread(buffer, 1, fileSize, inputFile)) > 0)
+        {
             fwrite(buffer, 1, bytesRead, outFile);
         }
 
@@ -196,70 +309,108 @@ void append_tar(const char *archiveFile, int numFiles, char *filesToAppend[]){
 
     fclose(outFile);
     verbose("Se agregó al paquete star: %s\n", archiveFile);
-
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int argIndex = 1;
     char *outputFile = NULL;
     int numFiles = 0;
     char *inputFiles[argc];
     bool create = false, extract = false, list = false, delete = false, update = false, append = false;
 
-    if (argc < 3) {
-        fprintf(stderr, "Comando: %s -c|-x|-t|-d|-u -f <archivoSalida> <archivo1> <archivo2> ... <archivoN>\n", argv[0]);
+    if (argc < 3)
+    {
+        fprintf(stderr, "Comando: %s -c|-x|-t|-d|-u|-v|-vv -f <archivoSalida> <archivo1> <archivo2> ... <archivoN>\n", argv[0]);
         exit(1);
     }
 
-    while (argIndex < argc) {
-        if (strcmp(argv[argIndex], "-c") == 0) {
+    while (argIndex < argc)
+    {
+        if (strcmp(argv[argIndex], "-c") == 0)
+        {
             create = true;
             argIndex++;
-        } else if (strcmp(argv[argIndex], "-x") == 0) {
+        }
+        else if (strcmp(argv[argIndex], "-x") == 0)
+        {
             extract = true;
             argIndex++;
-        } else if (strcmp(argv[argIndex], "-t") == 0) {
+        }
+        else if (strcmp(argv[argIndex], "-t") == 0)
+        {
             list = true;
             argIndex++;
-        } else if (strcmp(argv[argIndex], "-u") == 0) {
+        }
+        else if (strcmp(argv[argIndex], "-u") == 0)
+        {
             update = true;
             argIndex++;
-        } else if (strcmp(argv[argIndex], "-vv") == 0) {
+        }
+        else if (strcmp(argv[argIndex], "-vv") == 0)
+        {
             setStrongVerbose(true);
             setVerbose(true);
             argIndex++;
-        } else if (strcmp(argv[argIndex], "-v") == 0) {
+        }
+        else if (strcmp(argv[argIndex], "-v") == 0)
+        {
             setVerbose(true);
             argIndex++;
-        } else if (strcmp(argv[argIndex], "-r") == 0) {
+        }
+        else if (strcmp(argv[argIndex], "-r") == 0)
+        {
             append = true;
             argIndex++;
-        } else if (strcmp(argv[argIndex], "-f") == 0) {
+        }
+        else if (strcmp(argv[argIndex], "-f") == 0)
+        {
             argIndex++;
-            if (argIndex < argc) {
+            if (argIndex < argc)
+            {
                 outputFile = argv[argIndex];
                 argIndex++;
-            } else {
+            }
+            else
+            {
                 fprintf(stderr, "Error: Se esperaba un nombre de archivo después de -f\n");
                 exit(1);
             }
-        } else {
+        }
+        else if (strcmp(argv[argIndex], "-d") == 0)
+        {
+            delete = true;
+            argIndex++;
+        }
+        else
+        {
             inputFiles[numFiles++] = argv[argIndex];
             argIndex++;
         }
     }
 
-    if (create) {
+    if (create)
+    {
         create_tar(outputFile, numFiles, inputFiles);
-    } else if (extract) {
+    }
+    else if (extract)
+    {
         extract_tar(outputFile, numFiles, inputFiles);
-    } else if (list) {
+    }
+    else if (list)
+    {
         list_tar(outputFile);
-    } else if (delete) {
+    }
+    else if (delete)
+    {
         delete_tar(outputFile, numFiles, inputFiles);
-    } else if (update) {
+    }
+    else if (update)
+    {
         update_tar(outputFile, numFiles, inputFiles);
-    } else if (append) {
+    }
+    else if (append)
+    {
         append_tar(outputFile, numFiles, inputFiles);
     }
 
